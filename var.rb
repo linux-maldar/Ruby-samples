9e0b198195b39d6f24abcba6e5119531f917a45f
unknown <20056309@BLTSP02900.lnties.com>
2015-10-21 12:20:14 +0530

    adding new examples

---
+++ var.rb
@@ -0,0 +1,21 @@
+#!/usr/bin/ruby
+
+def test
+   i = 100
+   j = 200
+   k = 300
+   l = 400
+return i, j, k, l
+end
+
+def test1
+   a = 500
+   b = 600
+   c = 700
+   d = 800
+return a, b, c, d
+end
+var = test
+puts var
+v = test1
+puts v
